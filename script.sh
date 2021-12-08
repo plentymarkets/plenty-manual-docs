d6ae932f80fb69b2d22f6ef8ca98571f7b349347
Petrisor Frincu <petrisor.frincu@plentymarkets.com>
2021-12-08 13:53:43 +0200

    Implement Script for generating Antora

--- script.sh
+++ script.sh
@@ -10,6 +10,10 @@ fi
 
 git clone "https://github.com/plentymarkets/manual" gitRepo
 
+if [ -d build ]; then
+  rm -rf build
+fi
+
 if [ -d adocOutput ]; then
   rm -rf adocOutput
 fi
@@ -171,80 +175,6 @@ find adocOutput/en/modules/_includes/ -name '*.adoc' -exec sed -i -e -r 's/inclu
 #STARTING GENERATING THE MENU#
 
 echo '######################';
-echo '#    build nav DE    #';
+echo '#    build nav       #';
 echo '######################';
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install jq
-#jq '.willkommen' de.json
-echo 'name: manual\ntitle: plentymarkets Handbuch\nversion: main\nnav:\n- modules/ROOT/nav.adoc' > adocOutput/de/antora.yml
-
-for menuParent in $(jq -r 'keys_unsorted[]' de.json); do
-    #Create first levels
-    parentTitle=$(jq -r '."'${menuParent}'".title' de.json)
-    sudo bash -c 'echo "* xref:"'"${menuParent}"'".adoc["'"${parentTitle}"'"]" >> adocOutput/de/modules/ROOT/nav.adoc'
-
-    #create first children
-    for child in $(jq -r '."'${menuParent}'".children | keys_unsorted[]' de.json); do
-      title=$(jq -r '."'${menuParent}'".children."'${child}'".title' de.json)
-      sudo bash -c 'echo "** xref:"'"${menuParent}"'"/"'"${child}"'".adoc["'"${title}"'"]" >> adocOutput/de/modules/ROOT/nav.adoc'
-
-      #create second children
-      for secondChild in $(jq -r '."'${menuParent}'".children."'${child}'".children | keys_unsorted[]' de.json); do
-          secondChildTitle=$(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".title' de.json)
-          sudo bash -c 'echo "*** xref:"'"${menuParent}"'"/"'"${child}"'"/"'"${secondChild}"'".adoc["'"${secondChildTitle}"'"]" >> adocOutput/de/modules/ROOT/nav.adoc'
-
-          #create third children
-          for thirdChild in $(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".children | keys_unsorted[]' de.json); do
-              thirdChildTitle=$(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".children."'${thirdChild}'".title' de.json)
-              sudo bash -c 'echo "**** xref:"'"${menuParent}"'"/"'"${child}"'"/"'"${secondChild}"'"/"'"${thirdChild}"'".adoc["'"${thirdChildTitle}"'"]" >> adocOutput/de/modules/ROOT/nav.adoc'
-
-              #create fourth children
-                  for fourthChild in $(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".children."'${thirdChild}'".children | keys_unsorted[]' de.json); do
-                      fourthChildTitle=$(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".children."'${thirdChild}'".children."'${fourthChild}'".title' de.json)
-                      sudo bash -c 'echo "***** xref:"'"${menuParent}"'"/"'"${child}"'"/"'"${secondChild}"'"/"'"${thirdChild}"'"/"'"${fourthChild}"'".adoc["'"${fourthChildTitle}"'"]" >> adocOutput/de/modules/ROOT/nav.adoc'
-                  done
-
-          done
-
-      done
-
-    done
-done
-
-
-#EN MENU
-echo 'name: manual\ntitle: plentymarkets Handbuch\nversion: main\nnav:\n- modules/ROOT/nav.adoc' > adocOutput/en/antora.yml
-
-for menuParent in $(jq -r 'keys_unsorted[]' en.json); do
-    #Create first levels
-    parentTitle=$(jq -r '."'${menuParent}'".title' en.json)
-    sudo bash -c 'echo "* xref:"'"${menuParent}"'".adoc["'"${parentTitle}"'"]" >> adocOutput/en/modules/ROOT/nav.adoc'
-
-    #create first children
-    for child in $(jq -r '."'${menuParent}'".children | keys_unsorted[]' en.json); do
-      title=$(jq -r '."'${menuParent}'".children."'${child}'".title' en.json)
-      sudo bash -c 'echo "** xref:"'"${menuParent}"'"/"'"${child}"'".adoc["'"${title}"'"]" >> adocOutput/en/modules/ROOT/nav.adoc'
-
-      #create second children
-      for secondChild in $(jq -r '."'${menuParent}'".children."'${child}'".children | keys_unsorted[]' en.json); do
-          secondChildTitle=$(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".title' en.json)
-          sudo bash -c 'echo "*** xref:"'"${menuParent}"'"/"'"${child}"'"/"'"${secondChild}"'".adoc["'"${secondChildTitle}"'"]" >> adocOutput/en/modules/ROOT/nav.adoc'
-
-          #create third children
-          for thirdChild in $(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".children | keys_unsorted[]' en.json); do
-              thirdChildTitle=$(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".children."'${thirdChild}'".title' en.json)
-              sudo bash -c 'echo "**** xref:"'"${menuParent}"'"/"'"${child}"'"/"'"${secondChild}"'"/"'"${thirdChild}"'".adoc["'"${thirdChildTitle}"'"]" >> adocOutput/en/modules/ROOT/nav.adoc'
-
-              #create fourth children
-                  for fourthChild in $(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".children."'${thirdChild}'".children | keys_unsorted[]' en.json); do
-                      fourthChildTitle=$(jq -r '."'${menuParent}'".children."'${child}'".children."'${secondChild}'".children."'${thirdChild}'".children."'${fourthChild}'".title' en.json)
-                      sudo bash -c 'echo "***** xref:"'"${menuParent}"'"/"'"${child}"'"/"'"${secondChild}"'"/"'"${thirdChild}"'"/"'"${fourthChild}"'".adoc["'"${fourthChildTitle}"'"]" >> adocOutput/en/modules/ROOT/nav.adoc'
-                  done
-
-          done
-
-      done
-
-    done
-done
+sh generateNav.sh
