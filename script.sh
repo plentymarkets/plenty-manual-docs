8cea276695a99b7dfb1bd66fe1dc5b9e2e5cdf58
Petrisor Frincu <petrisor.frincu@plentymarkets.com>
2022-01-31 13:58:15 +0200

    dcj/223457 - fix internal links

--- script.sh
+++ script.sh
@@ -270,13 +270,13 @@ echo '######################';
 echo '#   Global actions   #';
 echo '######################';
 # Update links
-# find docs/ -name '*.adoc' -exec sed -i -E -e "s/<<([A-Za-zÀ-ž0-9\_\-]+)((\/)[^\s]+?)?\/([A-Za-zÀ-ž0-9\_\-]+)(#([A-Za-zÀ-ž0-9\_\-]+)?)?(,( ?)([A-Za-zÀ-ž\_\-\s]+)?)>>/xref:\1:\4.adoc\5\[\9\]/ig" {} \;
+#find docs/ -name '*.adoc' -exec sed -r -i -E "s/<<([a-zA-ZäöüÄÖÜß0-9\_\-]+)(\/.+)?\/([a-zA-ZäöüÄÖÜß0-9\_\-]+)(#([a-zA-ZäöüÄÖÜß0-9\_\-]+)?)?(, ?([ a-zA-ZäöüÄÖÜß0-9\_\-\(\)]*)?)>>/xref:\1:\3.adoc\4\[\7\]/g" {} \;
 
 # Update tabs class
 # find docs/ -name '*.adoc' -exec sed -i -r -e "s/\[\.tabs\]/\[tabs\]/ig" {} \;
 
 # Global actions for all files
-find docs/ -name '*.adoc' -exec sed -i -E -e "s/<<([A-Za-zÀ-ž0-9\_\-]+)((\/)[^\s]+?)?\/([A-Za-zÀ-ž0-9\_\-]+)(#([A-Za-zÀ-ž0-9\_\-]+)?)?(,( ?)([A-Za-zÀ-ž\_\-\s]+)?)>>/xref:\1:\4.adoc\5\[\9\]/ig;s/\[\.tabs\]/\[tabs\]/ig" {} \;
+find docs/ -name '*.adoc' -exec sed -r -i -E "s/<<([a-zA-ZäöüÄÖÜß0-9\_\-]+)(\/.+)?\/([a-zA-ZäöüÄÖÜß0-9\_\-]+)(#([a-zA-ZäöüÄÖÜß0-9\_\-]+)?)?(, ?([ a-zA-ZäöüÄÖÜß0-9\_\-\(\)]*)?)>>/xref:\1:\3.adoc\4\[\7\]/g;s/\[\.tabs\]/\[tabs\]/ig" {} \;
 
 # Update position header attribute
 find docs/*/pages/ -name '*.adoc' -exec sed -i -r -e "s/:position:\s0/:index:\sfalse/ig;s/:position:\s[0-9]{5,}/:index: false/ig" {} \;
